9e07f8c31275d9614802d455a6451e418b884e3e
Michael Handkins <michaelhandkins@gmail.com>
2020-12-16 18:59:16 -0600

    Initial commit

---
+++ The Dating App/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Dating App
+//
+//  Created by Michael Handkins on 12/16/20.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
