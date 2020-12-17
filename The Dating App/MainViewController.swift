89a22a6a3b11ad335214f1e036049a1e9f92ef19
Michael Handkins <michaelhandkins@gmail.com>
2020-12-16 19:14:21 -0600

    Add labels and buttons to main storyboard and hook them up as outlets in main view controller

---
+++ The Dating App/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  The Dating App
+//
+//  Created by Michael Handkins on 12/16/20.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var signInWithFacebook: UIButton!
+    @IBOutlet weak var signInWithGoogle: UIButton!
+    @IBOutlet weak var orLabel: UILabel!
+    @IBOutlet weak var createAccountButton: UIButton!
+    @IBOutlet weak var termsOfServiceLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
