eba358696035a9e626c83ce30e9ff59e1e2a3355
Michael Handkins <michaelhandkins@gmail.com>
2020-12-17 14:12:31 -0600

    Add customization to account creation page

--- The Dating App/MainViewController.swift
+++ The Dating App/MainViewController.swift
@@ -25,18 +25,47 @@ class MainViewController: UIViewController {
     func setupUI() {
         
         let title = "Create a new account"
-        let subtitle = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        let subtitle = "\n\n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do"
         
         let attributedText = NSMutableAttributedString(string: title, attributes:
             [NSAttributedString.Key.font : UIFont.init(name: "Didot", size: 28)!,
-             NSAttributedString.Key.foregroundColor : UIColor.brown])
+             NSAttributedString.Key.foregroundColor : UIColor.black])
         let attributedSubtitle = NSMutableAttributedString(string: subtitle, attributes:
             [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
-             NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 0.45)])
-        
+             NSAttributedString.Key.foregroundColor : UIColor.black])
         attributedText.append(attributedSubtitle)
         
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 5
+        
+        attributedText.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedText.length))
+        titleLabel.numberOfLines = 0
+        
+        
         titleLabel.attributedText = attributedText
+        
+        orLabel.text = "Or"
+        orLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        orLabel.textColor = UIColor(white: 0, alpha: 0.45)
+        orLabel.textAlignment = .center
+        
+        let attributedTermsText = NSMutableAttributedString(string: "By clicking \"Create a new account\" you agree to our ", attributes:
+            [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14),
+             NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 0.65)])
+        
+        let attributedSubTermsText = NSMutableAttributedString(string: "Terms of Service.", attributes:
+            [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 14),
+             NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 0.65)])
+        
+        attributedTermsText.append(attributedSubTermsText)
+        
+        termsOfServiceLabel.attributedText = attributedTermsText
+        termsOfServiceLabel.numberOfLines = 0
+        
+        
+        
+        
+        
         
         
         
