9d6cf8a2f0d7d527ce9ee07c0aa89804a0343e14
Michael Handkins <michaelhandkins@gmail.com>
2020-12-17 19:55:46 -0600

    Begin setting up Sign Up screen

---
+++ The Dating App/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  The Dating App
+//
+//  Created by Michael Handkins on 12/17/20.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
