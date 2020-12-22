bd72f4f338c2b5eb135ee0e565e53fcd1a6c476e
Michael Handkins <michaelhandkins@gmail.com>
2020-12-22 17:05:29 -0600

    Complete UI setup for sign up view

--- The Dating App/Controller/SignUpViewController+UI.swift
+++ The Dating App/Controller/SignUpViewController+UI.swift
@@ -25,23 +25,68 @@ extension SignUpViewController {
     }
     
     func setupFullnameTextField() {
+        fullnameContainerView.layer.borderWidth = 1
+        fullnameContainerView.layer.borderColor = UIColor(red: 210/255, green: 210/255, blue: 210/255, alpha: 1).cgColor
+        fullnameContainerView.layer.cornerRadius = 25
+        fullnameContainerView.clipsToBounds = true
         
+        fullnameTextField.borderStyle = .none
+        
+        let placeholderAttr = NSAttributedString(string: "Full Name", attributes: [NSAttributedString.Key.foregroundColor : UIColor(red: 170/255, green: 170/255, blue: 170/255, alpha: 1)])
+        
+        fullnameTextField.attributedPlaceholder = placeholderAttr
+        fullnameTextField.textColor = UIColor(red: 99/255, green: 99/255, blue: 99/255, alpha: 1)
     }
     
     func setupEmailTextField() {
+        emailContainerView.layer.borderWidth = 1
+        emailContainerView.layer.borderColor = UIColor(red: 210/255, green: 210/255, blue: 210/255, alpha: 1).cgColor
+        emailContainerView.layer.cornerRadius = 25
+        emailContainerView.clipsToBounds = true
         
+        emailTextField.borderStyle = .none
+        
+        let placeholderAttr = NSAttributedString(string: "Email Address", attributes: [NSAttributedString.Key.foregroundColor : UIColor(red: 170/255, green: 170/255, blue: 170/255, alpha: 1)])
+        
+        emailTextField.attributedPlaceholder = placeholderAttr
+        emailTextField.textColor = UIColor(red: 99/255, green: 99/255, blue: 99/255, alpha: 1)
     }
     
     func setupPasswordTextField() {
+        passwordContainerView.layer.borderWidth = 1
+        passwordContainerView.layer.borderColor = UIColor(red: 210/255, green: 210/255, blue: 210/255, alpha: 1).cgColor
+        passwordContainerView.layer.cornerRadius = 25
+        passwordContainerView.clipsToBounds = true
         
+        passwordTextField.borderStyle = .none
+        
+        let placeholderAttr = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor(red: 170/255, green: 170/255, blue: 170/255, alpha: 1)])
+        
+        passwordTextField.attributedPlaceholder = placeholderAttr
+        passwordTextField.textColor = UIColor(red: 99/255, green: 99/255, blue: 99/255, alpha: 1)
     }
     
     func setupSignUpButton() {
-        
+        signUpButton.setTitle("Create a new account", for: .normal)
+        signUpButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        signUpButton.backgroundColor = UIColor.black
+        signUpButton.layer.cornerRadius = 25
+        signUpButton.clipsToBounds = true
+        signUpButton.setTitleColor(.white, for: .normal)
     }
     
     func setupSignInButton() {
+        let attributedText = NSMutableAttributedString(string: "Already have an account? ", attributes:
+            [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+             NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 0.65)])
         
+        let attributedSubText = NSMutableAttributedString(string: "Sign In", attributes:
+            [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 18),
+             NSAttributedString.Key.foregroundColor : UIColor.black])
+        
+        attributedText.append(attributedSubText)
+        
+        signInButton.setAttributedTitle(attributedText, for: .normal)
     }
 
 }
